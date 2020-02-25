fe7b5890acce757d29b4d0c66c9ef2563f954e37
Hector Sanchez <20186183@itla.edu.do>
2020-02-25 18:42:43 -0400

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUD_Productos_Proveedores_ASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/ProveedoresM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_Productos_Proveedores_ASP.Models
+{
+    public class ProveedoresM
+    {
+    }
+}
